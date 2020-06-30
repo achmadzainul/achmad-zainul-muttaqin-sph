b87b5c5070dd9f7adf68147c0f22a72bed4b6ea3
achmadzainul <67637512+achmadzainul@users.noreply.github.com>
2020-06-30 19:56:43 +0700

    Add files via upload

---
+++ uts1/XWPFDocument.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uts1;
+
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+
+/**
+ *
+ * @author Asuz
+ */
+class XWPFDocument {
+
+    XWPFParagraph createParagraph() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void write(FileOutputStream out) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void write(FileOutputSream out) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
